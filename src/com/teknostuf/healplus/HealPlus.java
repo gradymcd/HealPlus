b455189f925e80925cd5376cb0169b5ccbd575be
gradymcd <gradymcd@verizon.net>
2013-03-10 18:30:05 -0400

    Beta 1.2.0
    + BurnPlus and BP commands- set fire ticks
    + Can now set health, food, or fireticks to 0
    - No longer automatically sets fire ticks with HealPlus or HP commands

--- src/com/teknostuf/healplus/HealPlus.java
+++ src/com/teknostuf/healplus/HealPlus.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HealPlus extends JavaPlugin {
-	public final Logger logger = Logger.getLogger("Minecraft");
+	public static final Logger logger = Logger.getLogger("Minecraft");
 	public static HealPlus plugin;
 
 	@Override
@@ -19,159 +19,128 @@ public class HealPlus extends JavaPlugin {
 		this.logger.info(pdfFile.getName() + " Has Been Disabled.");
 	}
 
-	@Override
+	/**
+     *
+     */
+    @Override
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled!");
 	}
 
+    /**
+     *
+     * @param sender
+     * @param cmd
+     * @param commandLabel
+     * @param args
+     * @return
+     */
+    @Override
 	public boolean onCommand(CommandSender sender, Command cmd,
 			String commandLabel, String[] args) {
 		Player player = (Player) sender;
+		Player targetPlayer = player.getServer().getPlayer(args[0]);
 
 		if (commandLabel.equalsIgnoreCase("healplus") || commandLabel.equalsIgnoreCase("hp") && player.hasPermission("hp.heal")){
 			if (args.length == 0) {
 				player.setHealth(20);
 				player.setFireTicks(0);
 				player.setFoodLevel(20);
-				player.sendMessage(ChatColor.GREEN + "You have been healed!");
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "You have been healed!");
 			} else if (args.length == 1) {
 				if (player.getServer().getPlayer(args[0]) != null) {
-					Player targetPlayer = player.getServer().getPlayer(args[0]);
 					targetPlayer.setHealth(20);
 					targetPlayer.setFireTicks(0);
-				}else if (args[0].equalsIgnoreCase("0")){
-					player.setHealth(0);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("1")){
 					player.setHealth(1);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("2")){
 					player.setHealth(2);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("3")){
 					player.setHealth(3);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("4")){
 					player.setHealth(4);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("5")){
 					player.setHealth(5);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("6")){
 					player.setHealth(6);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("7")){
 					player.setHealth(7);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("8")){
 					player.setHealth(8);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("9")){
 					player.setHealth(9);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("10")){
 					player.setHealth(10);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("11")){
 					player.setHealth(11);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("12")){
 					player.setHealth(12);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("13")){
 					player.setHealth(13);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("14")){
 					player.setHealth(14);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("15")){
 					player.setHealth(15);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("16")){
 					player.setHealth(16);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("17")){
 					player.setHealth(17);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("18")){
 					player.setHealth(18);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("19")){
 					player.setHealth(19);
-					player.setFireTicks(0);
 				} else if (args[0].equalsIgnoreCase("20")){
 					player.setHealth(20);
-					player.setFireTicks(0);
+                                } else if (args[0].equalsIgnoreCase("0")){
+                                        player.setHealth(0);
 				}
-				Player targetPlayer = player.getServer().getPlayer(args[0]);
-				player.sendMessage(ChatColor.GREEN + "You have healed " + targetPlayer.getDisplayName() + "!");
-				targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been healed by " + player.getDisplayName());
+				player.sendMessage(ChatColor.RED + "You have healed" + targetPlayer.getDisplayName() + "!");
+				targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been healed by" + player.getDisplayName() + "!");
 			} else if (args.length == 2) {
 				if (player.getServer().getPlayer(args[0]) != null) {
-					Player targetPlayer = player.getServer().getPlayer(args[0]);
 					if (args[1].equalsIgnoreCase("1")){
 						targetPlayer.setHealth(1);
-					}else if (args[1].equalsIgnoreCase("0")){
-						targetPlayer.setHealth(0);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("2")){
 						targetPlayer.setHealth(2);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("3")){
 						targetPlayer.setHealth(3);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("4")){
 						targetPlayer.setHealth(4);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("5")){
 						targetPlayer.setHealth(5);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("6")){
 						targetPlayer.setHealth(6);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("7")){
 						targetPlayer.setHealth(7);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("8")){
 						targetPlayer.setHealth(8);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("9")){
 						targetPlayer.setHealth(9);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("10")){
 						targetPlayer.setHealth(10);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("11")){
 						targetPlayer.setHealth(11);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("12")){
 						targetPlayer.setHealth(12);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("13")){
 						targetPlayer.setHealth(13);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("14")){
 						targetPlayer.setHealth(14);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("15")){
 						targetPlayer.setHealth(15);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("16")){
 						targetPlayer.setHealth(16);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("17")){
 						targetPlayer.setHealth(17);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("18")){
 						targetPlayer.setHealth(18);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("19")){
 						targetPlayer.setHealth(19);
-						targetPlayer.setFireTicks(0);
 					} else if (args[1].equalsIgnoreCase("20")){
 						targetPlayer.setHealth(20);
-						targetPlayer.setFireTicks(0);
+                                        } else if (args[1].equalsIgnoreCase("0")){
+                                                targetPlayer.setHealth(0);
 				}
 					player.sendMessage(ChatColor.GREEN + "You have healed " + targetPlayer.getDisplayName() + "!");
 					targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been healed by " + player.getDisplayName());
@@ -187,10 +156,7 @@ public class HealPlus extends JavaPlugin {
 				player.sendMessage(ChatColor.LIGHT_PURPLE + "You have been fed!");
 			} else if (args.length == 1) {
 				if (player.getServer().getPlayer(args[0]) != null) {
-					Player targetPlayer = player.getServer().getPlayer(args[0]);
-					targetPlayer.setFoodLevel(20);
-				}else if (args[0].equalsIgnoreCase("0")){
-					player.setFoodLevel(0);
+					player.setFoodLevel(20);
 				} else if (args[0].equalsIgnoreCase("1")){
 					player.setFoodLevel(1);
 				} else if (args[0].equalsIgnoreCase("2")){
@@ -231,17 +197,15 @@ public class HealPlus extends JavaPlugin {
 					player.setFoodLevel(19);
 				} else if (args[0].equalsIgnoreCase("20")){
 					player.setFoodLevel(20);
+                                } else if (args[0].equalsIgnoreCase("0")){
+                                        player.setFoodLevel(0);
 				}
-				Player targetPlayer = player.getServer().getPlayer(args[0]);
 				player.sendMessage(ChatColor.GREEN + "You have fed " + targetPlayer.getDisplayName() + "!");
-				targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been fed by " + player.getDisplayName() + "!");
+				targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been fed by" + player.getDisplayName() + "!");
 			} else if (args.length == 2) {
 				if (player.getServer().getPlayer(args[0]) != null) {
-					Player targetPlayer = player.getServer().getPlayer(args[0]);
 					if (args[1].equalsIgnoreCase("1")){
 						targetPlayer.setFoodLevel(1);
-					}else if (args[0].equalsIgnoreCase("0")){
-						targetPlayer.setFoodLevel(0);
 					} else if (args[1].equalsIgnoreCase("2")){
 						targetPlayer.setFoodLevel(2);
 					} else if (args[1].equalsIgnoreCase("3")){
@@ -280,9 +244,212 @@ public class HealPlus extends JavaPlugin {
 						targetPlayer.setFoodLevel(19);
 					} else if (args[1].equalsIgnoreCase("20")){
 						targetPlayer.setFoodLevel(20);
+                                        } else if (args[1].equalsIgnoreCase("0")){
+                                            targetPlayer.setFoodLevel(0);
 					}
 					player.sendMessage(ChatColor.GREEN + "You have fed " + targetPlayer.getDisplayName() + "!");
 					targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been fed by " + player.getDisplayName());
+				}
+				targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been fed by" + player.getDisplayName() + "!");
+			} else if (args.length == 2) {
+				if (player.getServer().getPlayer(args[0]) != null) {
+					if (args[1].equalsIgnoreCase("1")){
+						targetPlayer.setFoodLevel(1);
+					} else if (args[1].equalsIgnoreCase("2")){
+						targetPlayer.setFoodLevel(2);
+					} else if (args[1].equalsIgnoreCase("3")){
+						targetPlayer.setFoodLevel(3);
+					} else if (args[1].equalsIgnoreCase("4")){
+						targetPlayer.setFoodLevel(4);
+					} else if (args[1].equalsIgnoreCase("5")){
+						targetPlayer.setFoodLevel(5);
+					} else if (args[1].equalsIgnoreCase("6")){
+						targetPlayer.setFoodLevel(6);
+					} else if (args[1].equalsIgnoreCase("7")){
+						targetPlayer.setFoodLevel(7);
+					} else if (args[1].equalsIgnoreCase("8")){
+						targetPlayer.setFoodLevel(8);
+					} else if (args[1].equalsIgnoreCase("9")){
+						targetPlayer.setFoodLevel(9);
+					} else if (args[1].equalsIgnoreCase("10")){
+						targetPlayer.setFoodLevel(10);
+					} else if (args[1].equalsIgnoreCase("11")){
+						targetPlayer.setFoodLevel(11);
+					} else if (args[1].equalsIgnoreCase("12")){
+						targetPlayer.setFoodLevel(12);
+					} else if (args[1].equalsIgnoreCase("13")){
+						targetPlayer.setFoodLevel(13);
+					} else if (args[1].equalsIgnoreCase("14")){
+						targetPlayer.setFoodLevel(14);
+					} else if (args[1].equalsIgnoreCase("15")){
+						targetPlayer.setFoodLevel(15);
+					} else if (args[1].equalsIgnoreCase("16")){
+						targetPlayer.setFoodLevel(16);
+					} else if (args[1].equalsIgnoreCase("17")){
+						targetPlayer.setFoodLevel(17);
+					} else if (args[1].equalsIgnoreCase("18")){
+						targetPlayer.setFoodLevel(18);
+					} else if (args[1].equalsIgnoreCase("19")){
+						targetPlayer.setFoodLevel(19);
+					} else if (args[1].equalsIgnoreCase("20")){
+						targetPlayer.setFoodLevel(20);
+                                        } else if (args[1].equalsIgnoreCase("0")){
+						targetPlayer.setFoodLevel(0);
+					}
+					player.sendMessage(ChatColor.GREEN + "You have fed " + targetPlayer.getDisplayName() + "!");
+					targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been fed by " + player.getDisplayName());
+				}
+			} else {
+					player.sendMessage(ChatColor.RED + "That player is not online!");
+					}
+                		} if (commandLabel.equalsIgnoreCase("burnplus") || commandLabel.equalsIgnoreCase("bp") && player.hasPermission("hp.burn")) {
+			if (args.length == 0) {
+				player.setFireTicks(20);
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "You have been burned!");
+			} else if (args.length == 1) {
+				if (player.getServer().getPlayer(args[0]) != null) {
+					player.setFireTicks(20);
+				} else if (args[0].equalsIgnoreCase("1")){
+					player.setFireTicks(1);
+				} else if (args[0].equalsIgnoreCase("2")){
+					player.setFireTicks(2);
+				} else if (args[0].equalsIgnoreCase("3")){
+					player.setFireTicks(3);
+				} else if (args[0].equalsIgnoreCase("4")){
+					player.setFireTicks(4);
+				} else if (args[0].equalsIgnoreCase("5")){
+					player.setFireTicks(5);
+				} else if (args[0].equalsIgnoreCase("6")){
+					player.setFireTicks(6);
+				} else if (args[0].equalsIgnoreCase("7")){
+					player.setFireTicks(7);
+				} else if (args[0].equalsIgnoreCase("8")){
+					player.setFireTicks(8);
+				} else if (args[0].equalsIgnoreCase("9")){
+					player.setFireTicks(9);
+				} else if (args[0].equalsIgnoreCase("10")){
+					player.setFireTicks(10);
+				} else if (args[0].equalsIgnoreCase("11")){
+					player.setFireTicks(11);
+				} else if (args[0].equalsIgnoreCase("12")){
+					player.setFireTicks(12);
+				} else if (args[0].equalsIgnoreCase("13")){
+					player.setFireTicks(13);
+				} else if (args[0].equalsIgnoreCase("14")){
+					player.setFireTicks(14);
+				} else if (args[0].equalsIgnoreCase("15")){
+					player.setFireTicks(15);
+				} else if (args[0].equalsIgnoreCase("16")){
+					player.setFireTicks(16);
+				} else if (args[0].equalsIgnoreCase("17")){
+					player.setFireTicks(17);
+				} else if (args[0].equalsIgnoreCase("18")){
+					player.setFireTicks(18);
+				} else if (args[0].equalsIgnoreCase("19")){
+					player.setFireTicks(19);
+				} else if (args[0].equalsIgnoreCase("20")){
+					player.setFireTicks(20);
+                                } else if (args[0].equalsIgnoreCase("0")){
+					player.setFireTicks(0);
+				}
+				player.sendMessage(ChatColor.GREEN + "You have burned " + targetPlayer.getDisplayName() + "!");
+				targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been burned by" + player.getDisplayName() + "!");
+			} else if (args.length == 2) {
+				if (player.getServer().getPlayer(args[0]) != null) {
+					if (args[1].equalsIgnoreCase("1")){
+						targetPlayer.setFireTicks(1);
+					} else if (args[1].equalsIgnoreCase("2")){
+						targetPlayer.setFireTicks(2);
+					} else if (args[1].equalsIgnoreCase("3")){
+						targetPlayer.setFireTicks(3);
+					} else if (args[1].equalsIgnoreCase("4")){
+						targetPlayer.setFireTicks(4);
+					} else if (args[1].equalsIgnoreCase("5")){
+						targetPlayer.setFireTicks(5);
+					} else if (args[1].equalsIgnoreCase("6")){
+						targetPlayer.setFireTicks(6);
+					} else if (args[1].equalsIgnoreCase("7")){
+						targetPlayer.setFireTicks(7);
+					} else if (args[1].equalsIgnoreCase("8")){
+						targetPlayer.setFireTicks(8);
+					} else if (args[1].equalsIgnoreCase("9")){
+						targetPlayer.setFireTicks(9);
+					} else if (args[1].equalsIgnoreCase("10")){
+						targetPlayer.setFireTicks(10);
+					} else if (args[1].equalsIgnoreCase("11")){
+						targetPlayer.setFireTicks(11);
+					} else if (args[1].equalsIgnoreCase("12")){
+						targetPlayer.setFireTicks(12);
+					} else if (args[1].equalsIgnoreCase("13")){
+						targetPlayer.setFireTicks(13);
+					} else if (args[1].equalsIgnoreCase("14")){
+						targetPlayer.setFireTicks(14);
+					} else if (args[1].equalsIgnoreCase("15")){
+						targetPlayer.setFireTicks(15);
+					} else if (args[1].equalsIgnoreCase("16")){
+						targetPlayer.setFireTicks(16);
+					} else if (args[1].equalsIgnoreCase("17")){
+						targetPlayer.setFireTicks(17);
+					} else if (args[1].equalsIgnoreCase("18")){
+						targetPlayer.setFireTicks(18);
+					} else if (args[1].equalsIgnoreCase("19")){
+						targetPlayer.setFireTicks(19);
+					} else if (args[1].equalsIgnoreCase("20")){
+						targetPlayer.setFireTicks(20);
+                                        } else if (args[1].equalsIgnoreCase("0")){
+						targetPlayer.setFireTicks(0);
+					}
+					player.sendMessage(ChatColor.GREEN + "You have burned " + targetPlayer.getDisplayName() + "!");
+					targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been burned by " + player.getDisplayName());
+				}
+				targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been burned by" + player.getDisplayName() + "!");
+			} else if (args.length == 2) {
+				if (player.getServer().getPlayer(args[0]) != null) {
+					if (args[1].equalsIgnoreCase("1")){
+						targetPlayer.setFireTicks(1);
+					} else if (args[1].equalsIgnoreCase("2")){
+						targetPlayer.setFireTicks(2);
+					} else if (args[1].equalsIgnoreCase("3")){
+						targetPlayer.setFireTicks(3);
+					} else if (args[1].equalsIgnoreCase("4")){
+						targetPlayer.setFireTicks(4);
+					} else if (args[1].equalsIgnoreCase("5")){
+						targetPlayer.setFireTicks(5);
+					} else if (args[1].equalsIgnoreCase("6")){
+						targetPlayer.setFireTicks(6);
+					} else if (args[1].equalsIgnoreCase("7")){
+						targetPlayer.setFireTicks(7);
+					} else if (args[1].equalsIgnoreCase("8")){
+						targetPlayer.setFireTicks(8);
+					} else if (args[1].equalsIgnoreCase("9")){
+						targetPlayer.setFireTicks(9);
+					} else if (args[1].equalsIgnoreCase("10")){
+						targetPlayer.setFireTicks(10);
+					} else if (args[1].equalsIgnoreCase("11")){
+						targetPlayer.setFireTicks(11);
+					} else if (args[1].equalsIgnoreCase("12")){
+						targetPlayer.setFireTicks(12);
+					} else if (args[1].equalsIgnoreCase("13")){
+						targetPlayer.setFireTicks(13);
+					} else if (args[1].equalsIgnoreCase("14")){
+						targetPlayer.setFireTicks(14);
+					} else if (args[1].equalsIgnoreCase("15")){
+						targetPlayer.setFireTicks(15);
+					} else if (args[1].equalsIgnoreCase("16")){
+						targetPlayer.setFireTicks(16);
+					} else if (args[1].equalsIgnoreCase("17")){
+						targetPlayer.setFireTicks(17);
+					} else if (args[1].equalsIgnoreCase("18")){
+						targetPlayer.setFireTicks(18);
+					} else if (args[1].equalsIgnoreCase("19")){
+						targetPlayer.setFireTicks(19);
+					} else if (args[1].equalsIgnoreCase("20")){
+						targetPlayer.setFireTicks(20);
+                                        } else if (args[1].equalsIgnoreCase("0")){
+						targetPlayer.setFireTicks(0);
+					}
+					player.sendMessage(ChatColor.GREEN + "You have burned " + targetPlayer.getDisplayName() + "!");
+					targetPlayer.sendMessage(ChatColor.LIGHT_PURPLE + "You have been burned by " + player.getDisplayName());
 				}
 			} else {
 					player.sendMessage(ChatColor.RED + "That player is not online!");
